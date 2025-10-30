ec36a700b860bd2549b8549d59076174ef636a0b
Juman <jumandeka2004@gmail.com>
2025-10-30 23:02:21 +0530

    Added input field for entering city name and basic JavaScript handling

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("JavaScript file connected!");
+document.getElementById('search-btn').addEventListener('click', function() {
+  const city = document.getElementById('city-input').value;
+  console.log('City entered:', city);
+});
